b061b797075cd63d56360b08e80f32b9ab870856
piquel <piquel@piquel.fr>
2026-03-22 20:04:25 +0100

    add renderer package

---
+++ Engine/Source/renderer/src/lib.rs
@@ -0,0 +1,11 @@
+//! This crate contains everything to do with the main rendering logic
+
+/// The Renderer struct that holds all render state and is called upon to handle
+/// all rendering operations
+pub struct Renderer {}
+
+impl Renderer {
+    pub fn init() -> Self {
+        Self {}
+    }
+}
