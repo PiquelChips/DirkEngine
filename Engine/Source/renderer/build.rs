07c81ec246a7f9c4c64e4e87a6c99adcc66bebc1
piquel <piquel@piquel.fr>
2026-03-27 18:49:47 +0100

    setup better validation config

--- Engine/Source/renderer/build.rs
+++ Engine/Source/renderer/build.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("cargo:rustc-cfg=validation");
+    println!("cargo:rustc-check-cfg=cfg(validation)");
+
+    let profile = std::env::var("PROFILE").unwrap_or_default();
+    if profile != "release" {
+        println!("cargo:rustc-cfg=validation");
+    }
 }
