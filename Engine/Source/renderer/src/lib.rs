87f8cb8cf6af2dc691be50309559f12b8762e9c8
piquel <piquel@piquel.fr>
2026-04-02 22:34:15 +0200

    start new renderer init

--- Engine/Source/renderer/src/lib.rs
+++ Engine/Source/renderer/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 #[cfg(validation)]
 use ash::ext::debug_utils;
 use ash::{
@@ -5,6 +6,7 @@ use ash::{
     khr::{surface, swapchain},
     vk,
 };
+use log::info;
 
 mod errors;
 pub use errors::{RendererError, Result};
@@ -33,4 +35,33 @@ pub struct Renderer {
     debug_utils_loader: debug_utils::Instance,
     #[cfg(validation)]
     debug_messenger: vk::DebugUtilsMessengerEXT,
+}
+
+impl Renderer {
+    pub fn init(window: &platform::Window) -> Result<Self> {
+        info!("Intializing Vulkan...");
+
+        let entry = unsafe { Entry::load()? };
+
+        let app_info = vk::ApplicationInfo::default()
+            .application_name(c"DirkEngine")
+            .application_version(vk::make_api_version(0, 1, 0, 0))
+            .engine_name(c"DirkEngine")
+            .engine_version(vk::make_api_version(0, 1, 0, 0))
+            .api_version(vk::API_VERSION_1_3);
+
+        Ok(Self {
+            entry,
+            instance: (),
+            device: (),
+            queues: (),
+            physical_device: (),
+            surface: (),
+            properties: (),
+            surface_loader: (),
+            swapchain_loader: (),
+            debug_utils_loader: (),
+            debug_messenger: (),
+        })
+    }
 }
