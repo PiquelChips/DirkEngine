eb4e4c0fafe0b7d4dc24bc16341719ec4fd2921c
piquel <piquel@piquel.fr>
2026-04-03 23:58:42 +0200

    update comment

--- Engine/Source/resource_manager/src/lib.rs
+++ Engine/Source/resource_manager/src/lib.rs
@@ -2,6 +2,14 @@
 //! This struct handles all loading of data from the
 //! file system. It is intimately linked with the platform crate
 //! for platform-specifc resource loading.
+//!
+//! This crate can load glTF models into internal structs for upload by
+//! the renderer.
+//! It can also load textures for use by the renderer.
+//!
+//! In the future, it will also load sound and other assets. However,
+//! as these systems aren't implemented yet, the resource manager does
+//! not support loading them.
 
 /// This is the main struct that handles loading resources.
 pub struct ResourceManager {}
