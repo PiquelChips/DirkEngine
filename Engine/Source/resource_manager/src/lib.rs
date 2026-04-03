87ed36edac500988b060b3f14f01562ac2238221
piquel <piquel@piquel.fr>
2026-04-03 23:50:41 +0200

    add resource manager crate

---
+++ Engine/Source/resource_manager/src/lib.rs
@@ -0,0 +1,7 @@
+//! This crate has the ResourceManager struct.
+//! This struct handles all loading of data from the
+//! file system. It is intimately linked with the platform crate
+//! for platform-specifc resource loading.
+
+/// This is the main struct that handles loading resources.
+pub struct ResourceManager {}
