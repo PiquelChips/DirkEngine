09d16a7920ce1b8df239478b3570a17fef9d790f
piquel <piquel@piquel.fr>
2026-04-04 23:22:51 +0200

    add model cmd buffer recording

--- Engine/Source/renderer/src/structs.rs
+++ Engine/Source/renderer/src/structs.rs
@@ -8,6 +8,39 @@ pub struct Model {
 }
 
 impl Model {
+    pub fn record_cmd(&self, device: &Device, cmd: vk::CommandBuffer) {
+        // Assuming you've built a descriptor set layout with combined image samplers:
+        for prim in &self.primitives {
+            if let Some(mat_idx) = prim.material {
+                // from GpuPrimitive
+                let mat = &self.materials[mat_idx];
+
+                if let Some(tex_idx) = mat.base_color_texture() {
+                    let tex = &self.textures[*tex_idx];
+
+                    let image_info = vk::DescriptorImageInfo::default()
+                        .image_layout(vk::ImageLayout::SHADER_READ_ONLY_OPTIMAL)
+                        .image_view(tex.view)
+                        .sampler(tex.sampler);
+
+                    let write = vk::WriteDescriptorSet::default()
+                        // TODO: setup descriptor sets for the textures
+                        //.dst_set(descriptor_set)
+                        .dst_binding(0)
+                        .descriptor_type(vk::DescriptorType::COMBINED_IMAGE_SAMPLER)
+                        .image_info(std::slice::from_ref(&image_info));
+
+                    unsafe { device.update_descriptor_sets(&[write], &[]) };
+                }
+            }
+
+            unsafe {
+                device.cmd_bind_vertex_buffers(cmd, 0, &[prim.vertex_buffer], &[0]);
+                device.cmd_bind_index_buffer(cmd, prim.index_buffer, 0, vk::IndexType::UINT32);
+                device.cmd_draw_indexed(cmd, prim.index_count, 1, 0, 0, 0);
+            }
+        }
+    }
     pub fn destroy(&self, device: &Device) {
         for prim in &self.primitives {
             unsafe {
@@ -50,4 +83,5 @@ pub struct Primitive {
     pub index_buffer: vk::Buffer,
     pub index_buffer_memory: vk::DeviceMemory,
     pub index_count: u32,
+    pub material: Option<usize>,
 }
