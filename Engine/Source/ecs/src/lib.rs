79f7dc0340a661c065dc91b9d6cba48bcb053686
piquel <piquel@piquel.fr>
2026-04-05 15:37:44 +0200

    add generic safe querying for components & entities

--- Engine/Source/ecs/src/lib.rs
+++ Engine/Source/ecs/src/lib.rs
@@ -6,22 +6,49 @@ use components::*;
 /// An Entity is nothing more than a unique numeric identifier.
 pub type Entity = u32;
 
-#[derive(Default)]
-struct Components {
-    is_player: HashMap<Entity, IsPlayer>,
-    is_dead: HashMap<Entity, IsDead>,
-    renderables: HashMap<Entity, Renderable>,
+/// Component trait. Should not be implemented manually.
+/// Should be implemented by the [define_components] macro.
+/// NOT MANUALLY
+pub trait Component: 'static + Sized {
+    fn storage(components: &Components) -> &HashMap<Entity, Self>;
+    fn storage_mut(components: &mut Components) -> &mut HashMap<Entity, Self>;
 }
 
-impl Components {
-    /// Remove every component belonging to an entity.
-    fn remove_all(&mut self, entity: Entity) {
-        self.is_player.remove(&entity);
-        self.is_dead.remove(&entity);
-        self.renderables.remove(&entity);
-    }
+/// Collects all the structs that will be used as components. Will
+/// create the [Components] struct that is used to store the components
+/// for entities.
+macro_rules! define_components {
+    ( $( $C:ident ),* $(,)? ) => {
+
+        #[allow(non_snake_case)]
+        #[derive(Default)]
+        pub struct Components {
+            $( $C: HashMap<Entity, $C>, )*
+        }
+
+        impl Components {
+            fn remove_all(&mut self, entity: Entity) {
+                $( self.$C.remove(&entity); )*
+            }
+        }
+
+        $(
+            impl Component for $C {
+                fn storage(components: &Components) -> &HashMap<Entity, Self> {
+                    &components.$C
+                }
+                fn storage_mut(components: &mut Components) -> &mut HashMap<Entity, Self> {
+                    &mut components.$C
+                }
+            }
+        )*
+    };
 }
 
+define_components!(Renderable, IsPlayer, IsDead);
+
+/// Stores all the entities and their components. Handles state
+/// of all the entities in the world.
 #[derive(Default)]
 pub struct World {
     next_id: Entity,
@@ -30,6 +57,7 @@ pub struct World {
 }
 
 impl World {
+    /// Creates a new empty world.
     pub fn new() -> Self {
         Self::default()
     }
@@ -50,4 +78,69 @@ impl World {
     pub fn alive(&self) -> &[Entity] {
         &self.alive
     }
+
+    /// Attach (or overwrite) a component on an entity.
+    pub fn insert<C: Component>(&mut self, entity: Entity, component: C) {
+        C::storage_mut(&mut self.components).insert(entity, component);
+    }
+    /// Shared reference to a component; `None` if the entity lacks it.
+    pub fn get<C: Component>(&self, entity: Entity) -> Option<&C> {
+        C::storage(&self.components).get(&entity)
+    }
+    /// Mutable reference to a component; `None` if the entity lacks it.
+    pub fn get_mut<C: Component>(&mut self, entity: Entity) -> Option<&mut C> {
+        C::storage_mut(&mut self.components).get_mut(&entity)
+    }
+    /// Detach a single component from an entity (entity itself survives).
+    pub fn remove<C: Component>(&mut self, entity: Entity) {
+        C::storage_mut(&mut self.components).remove(&entity);
+    }
+
+    /// Queries all the components that have the specified component.
+    pub fn query_single<A: Component>(&self) -> Vec<Entity> {
+        self.alive
+            .iter()
+            .filter(|&e| A::storage(&self.components).contains_key(e))
+            .cloned()
+            .collect()
+    }
+    /// Queries all the components that have the two specified components.
+    pub fn query_double<A: Component, B: Component>(&self) -> Vec<Entity> {
+        self.alive
+            .iter()
+            .filter(|&e| {
+                A::storage(&self.components).contains_key(e)
+                    && B::storage(&self.components).contains_key(e)
+            })
+            .cloned()
+            .collect()
+    }
+    /// Queries all the components that have the three specified components.
+    pub fn query_triple<A: Component, B: Component, C: Component>(&self) -> Vec<Entity> {
+        self.alive
+            .iter()
+            .filter(|&e| {
+                A::storage(&self.components).contains_key(e)
+                    && B::storage(&self.components).contains_key(e)
+                    && C::storage(&self.components).contains_key(e)
+            })
+            .cloned()
+            .collect()
+    }
+    /// Queries all the components that have the four specified components.
+    pub fn query_quadruple<A: Component, B: Component, C: Component, D: Component>(
+        &self,
+    ) -> Vec<Entity> {
+        self.alive
+            .iter()
+            .filter(|&e| {
+                A::storage(&self.components).contains_key(e)
+                    && B::storage(&self.components).contains_key(e)
+                    && C::storage(&self.components).contains_key(e)
+                    && D::storage(&self.components).contains_key(e)
+            })
+            .cloned()
+            .collect()
+    }
+}
 }
