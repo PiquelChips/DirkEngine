e5778db77c818999ef9bc75d31f8e81bc89b5c84
piquel <piquel@piquel.fr>
2026-04-06 00:11:03 +0200

    start adding ubo & descriptor stuff to scene

--- Engine/Source/renderer/src/scene.rs
+++ Engine/Source/renderer/src/scene.rs
@@ -1,9 +1,9 @@
 use std::ffi::c_void;
 
-use ash::vk::{self, MemoryMapFlags};
+use ash::vk::{self};
 use world::{World, components};
 
-use crate::{Renderer, Result};
+use crate::{MAX_FRAMES_IN_FLIGHT, Renderer, Result};
 
 /// This scene is created from a [world::World].
 /// It should then be updated whenever the world is updated.
@@ -11,13 +11,26 @@ use crate::{Renderer, Result};
 /// Handles rendering all the [world::components::Renderable] objects
 /// of the world.
 pub struct Scene {
-    /// Each scene has its own command pool.
-    command_pool: vk::CommandPool,
     /// The entities to render.
     proxies: Vec<SceneProxy>,
     /// View matrix calculated from camera position.
     view: glam::Mat4,
     /// Projection matrix calculated from screen settings.
+    proj: glam::Mat4,
+
+    descriptor_pool: vk::DescriptorPool,
+    ubo: [UboData; MAX_FRAMES_IN_FLIGHT],
+}
+
+#[derive(Debug)]
+struct UboData {
+    buffer: vk::Buffer,
+    memory: vk::DeviceMemory,
+    mapped: *mut c_void,
+}
+
+struct SceneUbo {
+    view: glam::Mat4,
     proj: glam::Mat4,
 }
 
@@ -26,24 +39,82 @@ impl Scene {
     /// Constructs the renderer stuff like command pools, descriptor sets, ... from
     /// the [Renderer] and all world proxy stuff from [World].
     pub fn build(renderer: &Renderer, world: &World) -> Result<Self> {
-        // TODO: load all the models that are used by the scene proxies
-        let command_pool = {
-            let pool_info = vk::CommandPoolCreateInfo::default()
-                .queue_family_index(renderer.properties.queue_family_indices.graphics)
-                .flags(vk::CommandPoolCreateFlags::RESET_COMMAND_BUFFER);
+        let proxy_count = world
+            .query_double::<components::Renderable, components::Transform>()
+            .len() as u32;
 
-            unsafe { renderer.device.create_command_pool(&pool_info, None)? }
+        let pool_sizes = [
+            vk::DescriptorPoolSize {
+                ty: vk::DescriptorType::UNIFORM_BUFFER,
+                // scene UBOs + object UBOs, all × frames in flight
+                descriptor_count: (1 + proxy_count) * MAX_FRAMES_IN_FLIGHT as u32,
+            },
+            vk::DescriptorPoolSize {
+                ty: vk::DescriptorType::COMBINED_IMAGE_SAMPLER,
+                // rough upper bound on material textures
+                descriptor_count: proxy_count * MAX_FRAMES_IN_FLIGHT as u32,
+            },
+        ];
+
+        let pool_info = vk::DescriptorPoolCreateInfo::default()
+            .pool_sizes(&pool_sizes)
+            .max_sets((1 + proxy_count * 2) * MAX_FRAMES_IN_FLIGHT as u32);
+
+        let descriptor_pool = unsafe { renderer.device.create_descriptor_pool(&pool_info, None)? };
+
+        // Allocate scene-level sets (one per frame)
+        let layouts = [renderer.layouts.scene; MAX_FRAMES_IN_FLIGHT];
+        let alloc_info = vk::DescriptorSetAllocateInfo::default()
+            .descriptor_pool(descriptor_pool)
+            .set_layouts(&layouts);
+
+        let scene_desc_sets: [vk::DescriptorSet; MAX_FRAMES_IN_FLIGHT] = unsafe {
+            renderer
+                .device
+                .allocate_descriptor_sets(&alloc_info)?
+                .try_into()
+                .unwrap()
         };
 
+        // TODO: create scene UBO buffers, write descriptor sets, build proxies
+
+        let ubo: Vec<UboData> = (0..MAX_FRAMES_IN_FLIGHT)
+            .map(|_| {
+                let size = size_of::<SceneUbo>() as u64;
+                let (buffer, memory) = renderer.create_buffer(
+                    size,
+                    vk::BufferUsageFlags::UNIFORM_BUFFER,
+                    vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT,
+                )?;
+
+                let mapped = unsafe {
+                    renderer
+                        .device
+                        .map_memory(memory, 0, size, vk::MemoryMapFlags::empty())?
+                };
+
+                Ok(UboData {
+                    buffer,
+                    memory,
+                    mapped,
+                })
+            })
+            .collect::<Result<Vec<_>>>()?;
+        let ubo: [UboData; MAX_FRAMES_IN_FLIGHT] = ubo.try_into().unwrap();
+
+        // TODO: load all the models that are used by the scene proxies
         let (camera, camera_trans) = Self::get_camera(world);
 
         Ok(Self {
-            command_pool,
             proxies: Self::make_scene_proxies(renderer, world),
             view: camera_trans.view(),
             proj: camera.projection(),
+            descriptor_pool,
+            ubo,
         })
     }
+    // TODO: on tick, worlds should be sent to update scenes
+    #[allow(unused)]
     /// This function will reconstruct the internal world data with the new input world.
     /// This includes: [SceneProxy]s, view matrix & projection matrix.
     pub fn rebuild(&mut self, renderer: &Renderer, world: &World) {
