b9637baf11381dca0abfd25dd80b001f6b40ba08
piquel <piquel@piquel.fr>
2026-04-06 10:48:18 +0200

    setup render pass

--- Engine/Source/renderer/src/render_pass.rs
+++ Engine/Source/renderer/src/render_pass.rs
@@ -1,111 +1,59 @@
-use ash::{Device, vk};
+use ash::vk;
 
-use crate::{DescriptorLayouts, RendererProperties, Result, Vertex};
+use crate::Renderer;
 
 /// This struct holds the graphics pipeline & stuff.
 /// It can be called on to begin the pass (begin rendering,
 /// bind graphics pipeline, ...)
-pub struct RenderPass {
-    pipeline_layout: vk::PipelineLayout,
-    graphics_pipeline: vk::Pipeline,
-}
+pub struct RenderPass;
 
 impl RenderPass {
-    pub fn build(
-        device: &Device,
-        layouts: &DescriptorLayouts,
-        properties: &RendererProperties,
-    ) -> Result<Self> {
-        let set_layouts = [layouts.scene, layouts.object, layouts.material];
-        let layout_info = vk::PipelineLayoutCreateInfo::default().set_layouts(&set_layouts);
-        let pipeline_layout = unsafe { device.create_pipeline_layout(&layout_info, None)? };
+    pub fn begin(
+        renderer: &Renderer,
+        cmd: vk::CommandBuffer,
+        size: vk::Extent2D,
+        color: vk::ImageView,
+        out: vk::ImageView,
+        depth: vk::ImageView,
+    ) {
+        let color_attachement = vk::RenderingAttachmentInfo::default()
+            .load_op(vk::AttachmentLoadOp::CLEAR)
+            .store_op(vk::AttachmentStoreOp::STORE)
+            .image_layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+            .image_view(color)
+            .resolve_image_layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+            .resolve_mode(vk::ResolveModeFlags::AVERAGE)
+            .resolve_image_view(out)
+            .clear_value(vk::ClearValue {
+                color: vk::ClearColorValue {
+                    float32: [0., 0., 0., 1.],
+                },
+            });
 
-        let shader_stages = [
-            vk::PipelineShaderStageCreateInfo::default().stage(vk::ShaderStageFlags::VERTEX),
-            // TODO: load the shaders
-            //.module(vert)
-            //.name(main)
-            vk::PipelineShaderStageCreateInfo::default().stage(vk::ShaderStageFlags::FRAGMENT),
-            // TODO: load the shaders
-            //.module(frag)
-            //.name(main)
-        ];
+        let depth_info = vk::RenderingAttachmentInfo::default()
+            .load_op(vk::AttachmentLoadOp::CLEAR)
+            .store_op(vk::AttachmentStoreOp::DONT_CARE)
+            .image_layout(vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
+            .image_view(depth)
+            .clear_value(vk::ClearValue {
+                depth_stencil: vk::ClearDepthStencilValue {
+                    depth: 1.,
+                    stencil: 0,
+                },
+            });
 
-        let binding_description = Vertex::binding_description();
-        let attribute_description = Vertex::attribute_description();
-        let vertex_input_info = vk::PipelineVertexInputStateCreateInfo::default()
-            .vertex_binding_descriptions(std::slice::from_ref(&binding_description))
-            .vertex_attribute_descriptions(&attribute_description);
+        let rendering_info = vk::RenderingInfo::default()
+            .render_area(vk::Rect2D {
+                offset: vk::Offset2D { x: 0, y: 0 },
+                extent: size,
+            })
+            .color_attachments(std::slice::from_ref(&color_attachement))
+            .depth_attachment(&depth_info)
+            .layer_count(1);
 
-        let input_assembly = vk::PipelineInputAssemblyStateCreateInfo::default()
-            .topology(vk::PrimitiveTopology::TRIANGLE_LIST)
-            .primitive_restart_enable(false);
-
-        let dynamic_state = vk::PipelineDynamicStateCreateInfo::default()
-            .dynamic_states(&[vk::DynamicState::VIEWPORT, vk::DynamicState::SCISSOR]);
-        let viewport_state = vk::PipelineViewportStateCreateInfo::default()
-            .viewport_count(1)
-            .scissor_count(1);
-
-        let rasterizer = vk::PipelineRasterizationStateCreateInfo::default()
-            .depth_clamp_enable(false)
-            .rasterizer_discard_enable(false) // enabling this disables output to frame buffer
-            .polygon_mode(vk::PolygonMode::FILL)
-            .line_width(1.)
-            .cull_mode(vk::CullModeFlags::BACK)
-            .front_face(vk::FrontFace::COUNTER_CLOCKWISE)
-            .depth_bias_enable(false);
-
-        let multisampling = vk::PipelineMultisampleStateCreateInfo::default()
-            .sample_shading_enable(false)
-            .rasterization_samples(properties.msaa_samples);
-
-        let color_blend_attachment = vk::PipelineColorBlendAttachmentState::default()
-            .blend_enable(false)
-            .color_write_mask(vk::ColorComponentFlags::RGBA);
-
-        let color_blending = vk::PipelineColorBlendStateCreateInfo::default()
-            .logic_op_enable(true)
-            .attachments(std::slice::from_ref(&color_blend_attachment));
-
-        let depth_test_info = vk::PipelineDepthStencilStateCreateInfo::default()
-            .depth_test_enable(true)
-            .depth_write_enable(true)
-            .depth_compare_op(vk::CompareOp::LESS);
-
-        let mut pipeline_rendering_info = vk::PipelineRenderingCreateInfo::default()
-            .color_attachment_formats(std::slice::from_ref(&properties.surface_format.format))
-            .depth_attachment_format(properties.depth_format);
-
-        let pipeline_info = vk::GraphicsPipelineCreateInfo::default()
-            .stages(&shader_stages)
-            .vertex_input_state(&vertex_input_info)
-            .input_assembly_state(&input_assembly)
-            .viewport_state(&viewport_state)
-            .rasterization_state(&rasterizer)
-            .multisample_state(&multisampling)
-            .depth_stencil_state(&depth_test_info)
-            .color_blend_state(&color_blending)
-            .dynamic_state(&dynamic_state)
-            .layout(pipeline_layout)
-            .subpass(0)
-            .base_pipeline_handle(vk::Pipeline::null())
-            .base_pipeline_index(-1)
-            .push_next(&mut pipeline_rendering_info);
-
-        let graphics_pipeline = unsafe {
-            device
-                .create_graphics_pipelines(
-                    vk::PipelineCache::null(),
-                    std::slice::from_ref(&pipeline_info),
-                    None,
-                )
-                .map_err(|(_, err)| err)?[0]
-        };
-
-        Ok(Self {
-            graphics_pipeline,
-            pipeline_layout,
-        })
+        unsafe { renderer.device.cmd_begin_rendering(cmd, &rendering_info) };
+    }
+    pub fn end(renderer: &Renderer, cmd: vk::CommandBuffer) {
+        unsafe { renderer.device.cmd_end_rendering(cmd) }
     }
 }
