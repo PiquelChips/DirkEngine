975ef71b7f1b94d08f2f398c94c6288171f8ec01
piquel <piquel@piquel.fr>
2026-04-06 15:01:56 +0200

    add new shader util

--- Engine/Shaders/src/lib.rs
+++ Engine/Shaders/src/lib.rs
@@ -12,6 +12,16 @@ impl Shader {
     pub const fn code(&self) -> &[u8] {
         self.code
     }
+    pub fn code_as_u32(&self) -> Vec<u32> {
+        assert!(
+            self.code.len() % 4 == 0,
+            "SPIR-V size must be a multiple of 4"
+        );
+        self.code
+            .chunks_exact(4)
+            .map(|c| u32::from_le_bytes(c.try_into().unwrap()))
+            .collect()
+    }
     /// Returns the entrypoint of this shader
     pub const fn entrypoint(&self) -> &str {
         self.entrypoint
