75fd10adc2800610e58abcbe06b27a63957a3084
piquel <piquel@piquel.fr>
2026-04-07 21:33:50 +0200

    add event processing to engine

--- Engine/Source/engine/src/lib.rs
+++ Engine/Source/engine/src/lib.rs
@@ -10,6 +10,8 @@ pub struct Engine {
     is_requesting_exit: bool,
     exit_error: Option<anyhow::Error>,
     last_tick: Instant,
+
+    platform_consumer: events::Consumer<platform::PlatformEvent>,
 }
 
 impl Engine {
@@ -19,6 +21,8 @@ impl Engine {
 
         let mut event_manager = events::EventManager::new();
         let platform = platform::Platform::init(&mut event_manager).context("platform init")?;
+
+        let platform_consumer = event_manager.subscribe();
 
         /* A rough idea of the flow of the C++ Engine
          *
@@ -39,30 +43,28 @@ impl Engine {
             event_manager,
             exit_error: None,
             last_tick: Instant::now(),
+
+            platform_consumer,
         })
     }
     pub fn tick(&mut self) -> anyhow::Result<bool> {
         let delta_time = self.capture_delta_time();
-
-        if self.is_requesting_exit() {
-            return Ok(false);
-        }
-
         self.event_manager.dispatch_all();
 
-        self.platform.tick(delta_time);
-
+        self.process_events();
         if self.is_requesting_exit() {
             return Ok(false);
         }
+
+        self.platform.tick(delta_time);
 
         /*
          * World Tick
          * Main Viewport tick
          * Render
          */
-        self.render().context("tick: render")?;
-        Ok(true)
+        self.render().context("render")?;
+        Ok(!self.is_requesting_exit())
     }
 
     pub fn render(&self) -> anyhow::Result<()> {
@@ -86,6 +88,16 @@ impl Engine {
          * - Should cleanup and close all the log files
          */
         Ok(())
+    }
+    fn process_events(&mut self) {
+        let exit = self
+            .platform_consumer
+            .consume_all()
+            .find(|event| matches!(event, PlatformEvent::AppExit(_)));
+
+        if let Some(PlatformEvent::AppExit(_)) = exit {
+            self.exit(None);
+        }
     }
     pub fn is_requesting_exit(&self) -> bool {
         self.is_requesting_exit || self.exit_error.is_some()
