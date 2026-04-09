e5a3fbe2422f5f3d99f2f26392973869a8397108
piquel <piquel@piquel.fr>
2026-04-09 19:57:41 +0200

    remove unused window code

--- Engine/Source/platform/src/window.rs
+++ Engine/Source/platform/src/window.rs
@@ -1,7 +1,6 @@
 use log::debug;
 use winit::{
     dpi::PhysicalSize,
-    keyboard::ModifiersState,
     raw_window_handle::{HasDisplayHandle, HasWindowHandle},
     window::{Theme, WindowId},
 };
@@ -13,7 +12,6 @@ pub struct Window {
     /// If the window is completely hidden (minized or covered by another
     /// window)
     occluded: bool,
-    modifiers: ModifiersState,
 }
 
 impl Window {
@@ -22,7 +20,6 @@ impl Window {
             focused: false,
             theme: window.theme().unwrap_or(Theme::Dark),
             occluded: false,
-            modifiers: Default::default(),
             window,
         }
     }
@@ -39,24 +36,6 @@ impl Window {
     }
     pub fn size(&self) -> PhysicalSize<u32> {
         self.window.surface_size()
-    }
-    /// Update if window is focused. This only updates internal state, do
-    /// not call if you want to focus the window;
-    pub fn focused(&mut self, focused: bool) {
-        self.focused = focused;
-    }
-    pub fn set_draw_theme(&mut self, theme: Theme) {
-        self.theme = theme;
-        self.window.request_redraw();
-    }
-    pub fn set_occluded(&mut self, occluded: bool) {
-        self.occluded = occluded
-    }
-    pub fn set_modifiers(&mut self, modifiers: ModifiersState) {
-        self.modifiers = modifiers
-    }
-    pub fn get_modifiers(&mut self) -> &ModifiersState {
-        &self.modifiers
     }
 }
 
