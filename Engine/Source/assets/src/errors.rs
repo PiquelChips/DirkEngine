f2a025427a3dd331c8cf4e458dc9a77073033b84
piquel <piquel@piquel.fr>
2026-04-20 12:09:38 +0200

    add some more errors for asset loading

--- Engine/Source/assets/src/errors.rs
+++ Engine/Source/assets/src/errors.rs
@@ -8,4 +8,10 @@ pub enum Error {
     IoError(#[from] std::io::Error),
     #[error("error during .dirkasset JSON serialisation: {0}")]
     SerialisationError(#[from] serde_json::Error),
+    #[error("asset data has already been consumed")]
+    AlreadyConsumed,
+    #[error("asset not found in registry: {0}")]
+    NotFound(String),
+    #[error("asset {0} has wrong type for the requested load")]
+    TypeMismatch(String),
 }
