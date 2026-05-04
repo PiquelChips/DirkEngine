d663b53cca79c66d18579e37854701be8b63a97b
piquel <piquel@piquel.fr>
2026-05-04 19:27:07 +0200

    add world management API

--- Engine/Source/universe/src/lib.rs
+++ Engine/Source/universe/src/lib.rs
@@ -16,12 +16,40 @@ pub type Entity = u32;
 pub type WorldId = u32;
 
 /// This struct is the manager for all the worlds.
+#[derive(Default)]
 pub struct Universe {
     worlds: HashMap<WorldId, World>,
     next_id: WorldId,
 }
 
+impl Universe {
+    /// Creates a new empty [`Universe`].
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Returns an optional reference to the requested [`World`].
+    pub fn get_world(&self, world: WorldId) -> Option<&World> {
+        self.worlds.get(&world)
+    }
+    /// Returns an optional mutable reference to the requested [`World`].
+    pub fn get_world_mut(&mut self, world: WorldId) -> Option<&mut World> {
+        self.worlds.get_mut(&world)
+    }
+    /// Will create a new empty world & return its ID.
+    pub fn create_world(&mut self) -> WorldId {
+        // maybe some kind of world builder?
+        todo!("create a new world")
+    }
+    /// Will destroy the world & call all its destruction systems.
+    pub fn destroy_world(&mut self, world: WorldId) {
+        let _world = self.worlds.remove(&world);
+        todo!("call all the world destruction systems")
+    }
+}
+
 /// This is a world. It has entities and components.
+#[derive(Default)]
 pub struct World {
     id: WorldId,
     next_id: Entity,
@@ -31,4 +59,18 @@ pub struct World {
     /// guaranteed by the [`World`] API. Please make sure to add these
     /// properly internally.
     components: HashMap<TypeId, Box<dyn AnyComponent>>,
+}
+
+impl World {
+    /// Creates a new empty world with the specified ID.
+    fn new(id: WorldId) -> Self {
+        Self {
+            id,
+            ..Self::default()
+        }
+    }
+    /// Calls all the destruction [`System`]s on the world
+    fn destroy(&mut self) {
+        todo!("call all the world systems for destruction")
+    }
 }
