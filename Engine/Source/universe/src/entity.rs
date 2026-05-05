3bcbe9cd76e1f5e00b2e3bb13363bf985d0c13b7
piquel <piquel@piquel.fr>
2026-05-05 19:07:10 +0200

    update entity with docs & EntityBuilder

--- Engine/Source/universe/src/entity.rs
+++ Engine/Source/universe/src/entity.rs
@@ -1,2 +1,24 @@
+//! This struct holds everything to do with [`Entity`]s.
+//! Entities are just simple handles.
+//! You can spawn them by creating an entity builder
+
 /// A unique, opaque identifier for a spawned entity.
-pub type Entity = u32;
+#[derive(Clone, Copy, Debug, Default, Hash, Eq, PartialEq)]
+pub struct Entity(pub(crate) u32);
+
+impl Entity {
+    /// Returns an empty [`EntityBuilder`].
+    pub fn builder() -> EntityBuilder {
+        EntityBuilder::new()
+    }
+}
+
+/// A builder struct to create a new entity. Allows adding of components.
+#[derive(Default)]
+pub struct EntityBuilder;
+
+impl EntityBuilder {
+    fn new() -> Self {
+        Self::default()
+    }
+}
