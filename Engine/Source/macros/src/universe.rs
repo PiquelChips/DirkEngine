4801de6b0447026a7080074e79e369fe8d671d5b
piquel <piquel@piquel.fr>
2026-05-05 20:26:56 +0200

    greatly simplify universe system macro

--- Engine/Source/macros/src/universe.rs
+++ Engine/Source/macros/src/universe.rs
@@ -16,7 +16,6 @@ pub fn generate_system_code(trait_def: &ItemTrait) -> syn::Result<TokenStream> {
     let trait_name = &trait_def.ident;
     let any_name = format_ident!("Any{trait_name}");
     let storage = format_ident!("{trait_name}Storage");
-    let handle = format_ident!("{trait_name}Handle");
 
     let fns: Vec<_> = trait_def
         .items
@@ -48,38 +47,24 @@ pub fn generate_system_code(trait_def: &ItemTrait) -> syn::Result<TokenStream> {
         // Re-emit the original trait unchanged.
         #trait_def
 
-        // Any trait
         trait #any_name { #(#any_sigs)* }
 
         impl<T: #trait_name> #any_name for T { #(#blanket_methods)* }
 
-        // Storage
-
-        // TODO: see about using a slotmap
         #[derive(Default)]
         pub(crate) struct #storage {
-            systems: ::std::collections::HashMap<#handle, Box<dyn #any_name>>
+            systems: ::std::vec::Vec<Box<dyn #any_name>>
         }
 
         impl #storage {
             pub fn new() -> Self { Self::default() }
 
-            pub fn insert<S: #trait_name>(&mut self, system: S) -> #handle {
-                todo!("insert into storage")
-            }
-
-            pub fn remove(&mut self, handle: #handle) {
-                todo!("remove from storage")
+            pub fn add<S: #trait_name>(&mut self, system: S) {
+                self.systems.push(Box::new(system));
             }
 
             // TODO: looping over all the traits
         }
-
-        #[derive(Clone, Copy)]
-        #[doc = concat!("Handle type for [`", stringify!(#trait_name), "`]")]
-        #[doc = "This allows subsystems to have a reference to their systems
-        in case they need to remove them."]
-        pub struct #handle(u32);
     })
 }
 
