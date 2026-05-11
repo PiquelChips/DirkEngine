c24c9d33b8302016b3f27c35340ceb6df0d8facc
piquel <piquel@piquel.fr>
2026-05-11 20:51:53 +0200

    remove extra tick call

--- Engine/Source/universe/src/lib.rs
+++ Engine/Source/universe/src/lib.rs
@@ -58,10 +58,6 @@ impl Universe {
             .for_each(|system| system.tick(self, delta_time));
 
         self.worlds.values().for_each(|world| {
-            self.universe_systems
-                .iter()
-                .for_each(|system| system.tick(self, delta_time));
-
             self.ticking_systems.iter().for_each(|system| {
                 // This allocates a new [`Vec`] per [`TickingSystem`] per tick.
                 // TODO: optimise this. IDK how tho
