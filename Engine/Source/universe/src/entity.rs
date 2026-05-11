33f5666c80f8864a6075ef44755e42dd41b53504
piquel <piquel@piquel.fr>
2026-05-11 20:51:05 +0200

    fix comments

--- Engine/Source/universe/src/entity.rs
+++ Engine/Source/universe/src/entity.rs
@@ -48,6 +48,9 @@ impl EntityBuilder {
     }
 
     /// Adds a [`Component`] to this entity.
+    ///
+    /// If adding multiple components of the same type, only the last call
+    /// to with_component will be kept.
     #[must_use]
     pub fn with_component<C: Component>(mut self, component: C) -> Self {
         self.components
