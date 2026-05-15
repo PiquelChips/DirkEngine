997dbc610fe4b64aed8134cda0057742123b1d79
piquel <piquel@piquel.fr>
2026-05-15 21:04:48 +0200

    add dirk_platform readme

--- crates/dirk_platform/src/lib.rs
+++ crates/dirk_platform/src/lib.rs
@@ -1,9 +1,4 @@
-//! This crate has all the platform level functionnality. It should be the
-//! only place where any kind of platform dependent code or `#[cfg()]` attributes
-//! should be used. This allows us to create a central platform API for eaiser
-//! development.
-//!
-//! The `DirkEngine`'s platform API is build on the winit crate.
+#![doc = include_str!("../README.md")]
 
 use std::{collections::HashMap, time::Duration};
 
