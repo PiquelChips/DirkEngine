a6f2421ba218d37fddc608cf02620caea2d71b5f
piquel <piquel@piquel.fr>
2026-05-15 21:02:45 +0200

    add dirk_build readme

---
+++ crates/dirk_build/README.md
@@ -0,0 +1,5 @@
+# build
+
+This crate has a bunch of utilities for the engine build scripts.
+This mainly avoids duplicating the code of stuff like platform
+configuration and the editor.
--- crates/dirk_build/src/lib.rs
+++ crates/dirk_build/src/lib.rs
@@ -1,6 +1,4 @@
-//! This crate has a bunch of utilities for the engine build scripts.
-//! This mainly avoids duplicating the code of stuff like platform
-//! configuration and the editor.
+#![doc = include_str!("../README.md")]
 
 use std::{collections::HashMap, path::PathBuf};
 
