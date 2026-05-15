639b175c82a5a53c1e745d83f7525e21011039d7
Piquel <piquel@piquel.fr>
2026-05-15 17:00:09 +0200

    feat: the Universe (#34)
    This PR adds the `universe` crate, a complete refactor of the ECS
    system.
    
    There are now systems which can be registered on Universe intialization.
    All changes are done through command buffers for proper lifecycle calls.
    
    ## Changes
    
    - `macros`: A `System` derive macro for the base system trait.
    - `macros`: add internal `system_trait` proc macro to generate code in
    the Universe crate. It shouldn't be used anywhere else.
    - integrate with engine systems: see #37 
    
    ## TODO
    
    - [x] `System` derive macro to populate name
    - [x] Make `entity` & `world` modules public
    - [x] Unit tests & integration tests

---
+++ Engine/Source/universe/src/entity.rs
@@ -0,0 +1,65 @@
+//! This struct holds everything to do with [`Entity`]s.
+//! Entities are just simple handles.
+//! You can spawn them by creating an entity builder
+
+use std::{
+    any::TypeId,
+    collections::HashMap,
+    ops::{Add, AddAssign},
+};
+
+use crate::components::{AnyComponent, Component};
+
+/// A unique, opaque identifier for a spawned entity.
+#[derive(Clone, Copy, Debug, Default, Hash, Eq, PartialEq)]
+pub struct Entity(u64);
+
+impl Entity {
+    /// Returns an empty [`EntityBuilder`].
+    #[must_use]
+    pub fn builder() -> EntityBuilder {
+        EntityBuilder::new()
+    }
+    /// Returns the raw entity ID
+    #[must_use]
+    pub fn raw(self) -> u64 {
+        self.0
+    }
+}
+
+impl Add<u64> for Entity {
+    type Output = Self;
+    fn add(self, rhs: u64) -> Self::Output {
+        Self(self.0 + rhs)
+    }
+}
+
+impl AddAssign<u64> for Entity {
+    fn add_assign(&mut self, rhs: u64) {
+        self.0 += rhs;
+    }
+}
+
+/// A builder struct to create a new entity. Allows adding of components.
+#[derive(Default)]
+pub struct EntityBuilder {
+    pub(crate) components: HashMap<TypeId, Box<dyn AnyComponent>>,
+}
+
+impl EntityBuilder {
+    #[must_use]
+    fn new() -> Self {
+        Self::default()
+    }
+
+    /// Adds a [`Component`] to this entity.
+    ///
+    /// If adding multiple components of the same type, only the last call
+    /// to `with_component` will be kept.
+    #[must_use]
+    pub fn with_component<C: Component>(mut self, component: C) -> Self {
+        self.components
+            .insert(TypeId::of::<C>(), Box::new(component));
+        self
+    }
+}
---
+++ Engine/Source/universe/src/world.rs
@@ -0,0 +1,97 @@
+use std::{
+    collections::HashSet,
+    fmt::Display,
+    ops::{Add, AddAssign},
+};
+
+use crate::{Entity, EntityBuilder};
+
+/// An identifier that distinguishes multiple [`World`] instances from each other.
+#[derive(Clone, Copy, Debug, Default, Hash, Eq, PartialEq)]
+pub struct WorldId(u32);
+
+impl WorldId {
+    /// Returns the raw entity ID
+    #[must_use]
+    pub fn raw(self) -> u32 {
+        self.0
+    }
+}
+
+impl Display for WorldId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Add<u32> for WorldId {
+    type Output = Self;
+    fn add(self, rhs: u32) -> Self::Output {
+        Self(self.0 + rhs)
+    }
+}
+
+impl AddAssign<u32> for WorldId {
+    fn add_assign(&mut self, rhs: u32) {
+        self.0 += rhs;
+    }
+}
+
+/// This is a world. It has entities and components.
+pub struct World {
+    id: WorldId,
+    name: String,
+    pub(crate) alive: HashSet<Entity>,
+}
+
+impl World {
+    /// Returns a [`WorldBuilder`].
+    #[must_use]
+    pub fn builder(name: impl Into<String>) -> WorldBuilder {
+        WorldBuilder::new(name)
+    }
+    /// Creates an empty world with a name & id.
+    #[must_use]
+    pub(crate) fn new(id: WorldId, name: String) -> Self {
+        Self {
+            id,
+            name,
+            alive: HashSet::new(),
+        }
+    }
+    /// Returns the [`WorldId`] of the [`World`].
+    #[must_use]
+    pub fn id(&self) -> WorldId {
+        self.id
+    }
+    /// Returns the name of the [`World`].
+    #[must_use]
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+/// Builder struct for [`World`].
+#[derive(Default)]
+pub struct WorldBuilder {
+    pub(crate) name: String,
+    pub(crate) entities: Vec<EntityBuilder>,
+}
+
+impl WorldBuilder {
+    /// Creates a new empty [`WorldBuilder`].
+    #[must_use]
+    fn new(name: impl Into<String>) -> Self {
+        Self {
+            name: name.into(),
+            ..Self::default()
+        }
+    }
+
+    /// Adds an [`Entity`] that will be spawned on [`World`] creation.
+    #[must_use]
+    pub fn with_entity(mut self, entity: EntityBuilder) -> Self {
+        self.entities.push(entity);
+        self
+    }
+}
