f978262f718c6921943e82c24e648196a9f1c89c
piquel <piquel@piquel.fr>
2026-05-15 17:14:38 +0200

    remove unused file

--- Engine/Source/renderer/src/proxy/types.rs
+++
@@ -1,9 +0,0 @@
-//! This module has all the proxy types. These are the render representations
-//! of the components needed for rendering
-
-use ash::vk;
-use platform::WindowId;
-use universe::{Entity, WorldId};
-use world::player::PlayerId;
-
-use crate::{MAX_FRAMES_IN_FLIGHT, resources::buffer::UniformBuffer};
