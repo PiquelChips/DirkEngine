5e4d8fb219e8efbb3759355bd9f0778f1ce17ff1
piquel <piquel@piquel.fr>
2026-05-15 21:09:29 +0200

    add dirk_utils README

--- crates/dirk_utils/src/lib.rs
+++ crates/dirk_utils/src/lib.rs
@@ -1,7 +1,4 @@
-//! This is a utility crate.
-//! It has basic utilities for use throughout the engine.
-//! No actual engine systems live in this crate. It just has
-//! many small features, functions and structures.
+#![doc = include_str!("../README.md")]
 
 use std::path::{Path, PathBuf};
 
