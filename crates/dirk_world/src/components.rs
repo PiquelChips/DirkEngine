3c2c41e9a6c18829ddf346ec3cced579f02def73
piquel <piquel@piquel.fr>
2026-05-15 19:23:58 +0200

    fmt

--- crates/dirk_world/src/components.rs
+++ crates/dirk_world/src/components.rs
@@ -174,7 +174,11 @@ impl Transform {
                 forward
             );
         }
-        Mat4::look_at_lh(self.location, self.location + forward, dirk_utils::UP_DIRECTION)
+        Mat4::look_at_lh(
+            self.location,
+            self.location + forward,
+            dirk_utils::UP_DIRECTION,
+        )
     }
 }
 
