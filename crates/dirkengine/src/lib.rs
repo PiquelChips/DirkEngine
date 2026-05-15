28ae96d6ad6205ac9f93acf51cff503c390fff40
piquel <piquel@piquel.fr>
2026-05-15 22:05:24 +0200

    inline all renderer docs

--- crates/dirkengine/src/lib.rs
+++ crates/dirkengine/src/lib.rs
@@ -3,11 +3,19 @@
 
 pub mod engine;
 
+#[doc(inline)]
 pub use dirk_assets as assets;
+#[doc(inline)]
 pub use dirk_events as events;
+#[doc(inline)]
 pub use dirk_logging as logging;
+#[doc(inline)]
 pub use dirk_platform as platform;
+#[doc(inline)]
 pub use dirk_renderer as renderer;
+#[doc(inline)]
 pub use dirk_universe as universe;
+#[doc(inline)]
 pub use dirk_utils as utils;
+#[doc(inline)]
 pub use dirk_world as world;
