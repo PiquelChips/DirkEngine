b0cd50c1689e6326210e69ce36413a1116ac79cb
piquel <piquel@piquel.fr>
2026-05-15 18:12:48 +0200

    export everything from dirkengine

--- crates/dirkengine/src/lib.rs
+++ crates/dirkengine/src/lib.rs
@@ -1,3 +1,13 @@
 #![doc = include_str!("../../../README.md")]
 
 pub mod engine;
+
+pub use dirk_assets as assets;
+pub use dirk_build as build;
+pub use dirk_events as events;
+pub use dirk_logging as logging;
+pub use dirk_platform as platform;
+pub use dirk_renderer as renderer;
+pub use dirk_universe as universe;
+pub use dirk_utils as utils;
+pub use dirk_world as world;
