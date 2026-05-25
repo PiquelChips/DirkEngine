ec7d6879ecc165b94d1f5b8e008f25032e56691f
piquel <piquel@piquel.fr>
2026-05-25 17:31:15 +0200

    move common event types to separate module

---
+++ crates/dirk_events/src/common.rs
@@ -0,0 +1,20 @@
+//! This module contains common event types used throughout the engine.
+
+use crate::Event;
+
+/// An event to request to the engine to exit.
+///
+/// This event is used by various engine systems.
+/// It can be used by the platform to signal to the engine that the windows
+/// have all been closed. It is also used when users manually exit the engine.
+#[derive(Debug, Clone, Event)]
+#[event("App exit requested: {0}")]
+pub struct AppExit(pub String);
+
+/// An event run at the beginning of every tick.
+///
+/// This event contains the frame number.
+/// Used for thread synchronization.
+#[derive(Debug, Clone, Event)]
+#[event("Begin frame number {0}")]
+pub struct BeginFrame(pub u64);
--- crates/dirk_events/src/lib.rs
+++ crates/dirk_events/src/lib.rs
@@ -11,6 +11,9 @@ use tracing::trace;
 
 use dirk_threads::WorkerPool;
 
+mod common;
+pub use common::*;
+
 mod tests;
 
 /// The marker trait that every event type must implement.
@@ -561,20 +564,3 @@ impl<T: Event> std::fmt::Debug for Consumer<T> {
         f.debug_struct("Consumer").finish_non_exhaustive()
     }
 }
-
-/// An event to request to the engine to exit.
-///
-/// This event is used by various engine systems.
-/// It can be used by the platform to signal to the engine that the windows
-/// have all been closed. It is also used when users manually exit the engine.
-#[derive(Debug, Clone, Event)]
-#[event("App exit requested: {0}")]
-pub struct AppExit(pub String);
-
-/// An event run at the beginning of every tick.
-///
-/// This event contains the frame number.
-/// Used for thread synchronization.
-#[derive(Debug, Clone, Event)]
-#[event("Begin frame number {0}")]
-pub struct BeginFrame(pub u64);
