f52b90cafd38b717881b27bacd70e1dee9241850
piquel <piquel@piquel.fr>
2026-06-01 17:07:07 +0200

    add basic engine skeleton system

---
+++ crates/dirk_engine/src/errors.rs
@@ -0,0 +1,7 @@
+//! This module contains all engine related errors.
+
+/// All other errors that could be produced by the engine at runtime.
+pub enum EngineError {}
+
+/// Any error that could occur during build.
+pub enum EngineBuildError {}
--- crates/dirk_engine/src/lib.rs
+++ crates/dirk_engine/src/lib.rs
@@ -1,3 +1,34 @@
 //! This module holds all the traits & struct for the engine.
 
 pub mod events;
+
+pub mod errors;
+
+pub mod build;
+use build::EngineBuilder;
+
+use crate::errors::{EngineBuildError, EngineError};
+
+/// The main engine object.
+///
+/// Create an [`Engine`] with the [`EngineBuilder`] to specifiy modules &
+/// subsystems that you want in the engine.
+pub struct Engine {}
+
+impl Engine {
+    /// Returns a [`EngineBuilder`]. This struct allows you to specify
+    /// the different modules & subsystems.
+    pub fn builder() -> EngineBuilder {
+        EngineBuilder {}
+    }
+
+    /// Actually runs the engine. This is a blocking loop that will not
+    /// exit until exit is requested or an error occurs.
+    pub fn run(self) -> Result<(), EngineError> {
+        Ok(())
+    }
+
+    fn build(builder: EngineBuilder) -> Result<Self, EngineBuildError> {
+        Ok(Self {})
+    }
+}
