f7dd3682629eac6c01ab6793a043759d5228e452
piquel <piquel@piquel.fr>
2026-06-03 22:24:07 +0200

    remove color from shaders

--- crates/dirk_renderer/shaders/src/lib.rs
+++ crates/dirk_renderer/shaders/src/lib.rs
@@ -15,22 +15,19 @@ pub fn main_vs(
     #[spirv(uniform, descriptor_set = 0, binding = 0)] scene: &SceneUbo,
     #[spirv(uniform, descriptor_set = 1, binding = 1)] proxy: &ProxyUbo,
     #[spirv(location = 0)] in_position: Vec3,
-    #[spirv(location = 1)] in_color: Vec3,
+    #[spirv(location = 1)] _in_normal: Vec3,
     #[spirv(location = 2)] in_tex_coord: Vec2,
     #[spirv(position)] out_position: &mut Vec4,
-    #[spirv(location = 0)] frag_color: &mut Vec3,
-    #[spirv(location = 1)] frag_tex_coord: &mut Vec2,
+    #[spirv(location = 0)] frag_tex_coord: &mut Vec2,
 ) {
     *out_position = scene.proj * scene.view * proxy.model * in_position.extend(1.0);
-    *frag_color = in_color;
     *frag_tex_coord = in_tex_coord;
 }
 
 #[spirv(fragment)]
 pub fn main_fs(
     #[spirv(descriptor_set = 2, binding = 2)] tex_sampler: &SampledImage<Image2d>,
-    #[spirv(location = 0)] _frag_color: Vec3,
-    #[spirv(location = 1)] frag_tex_coord: Vec2,
+    #[spirv(location = 0)] frag_tex_coord: Vec2,
     #[spirv(location = 0)] out_color: &mut Vec4,
 ) {
     *out_color = tex_sampler.sample(frag_tex_coord);
