e7153e870663f5628b274a5040d79eebdbf0dd10
piquel <piquel@piquel.fr>
2026-06-04 18:54:50 +0200

    update shader to use normal for diffuse

--- crates/dirk_renderer/shaders/src/lib.rs
+++ crates/dirk_renderer/shaders/src/lib.rs
@@ -15,20 +15,24 @@ pub fn main_vs(
     #[spirv(uniform, descriptor_set = 0, binding = 0)] scene: &SceneUbo,
     #[spirv(uniform, descriptor_set = 1, binding = 0)] proxy: &ProxyUbo,
     #[spirv(location = 0)] in_position: Vec3,
-    #[spirv(location = 1)] _in_normal: Vec3,
+    #[spirv(location = 1)] in_normal: Vec3,
     #[spirv(location = 2)] in_tex_coord: Vec2,
     #[spirv(position)] out_position: &mut Vec4,
     #[spirv(location = 0)] frag_tex_coord: &mut Vec2,
+    #[spirv(location = 1)] frag_normal: &mut Vec3,
 ) {
     *out_position = scene.proj * scene.view * proxy.model * in_position.extend(1.0);
     *frag_tex_coord = in_tex_coord;
+    *frag_normal = in_normal;
 }
 
 #[spirv(fragment)]
 pub fn main_fs(
     #[spirv(descriptor_set = 2, binding = 0)] tex_sampler: &SampledImage<Image2d>,
     #[spirv(location = 0)] frag_tex_coord: Vec2,
+    #[spirv(location = 1)] frag_normal: Vec3,
     #[spirv(location = 0)] out_color: &mut Vec4,
 ) {
-    *out_color = tex_sampler.sample(frag_tex_coord);
+    let diffuse = 0.35 + 0.65 * frag_normal.z.abs();
+    *out_color = tex_sampler.sample(frag_tex_coord) * Vec4::new(diffuse, diffuse, diffuse, 1.0);
 }
