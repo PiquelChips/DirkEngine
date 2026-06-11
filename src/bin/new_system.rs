d20d5f8c32a53fae6e0cc47cb18872e6d8b97951
piquel <piquel@piquel.fr>
2026-06-11 17:43:45 +0200

    temp: dirkengine-new binary target to test new system

---
+++ src/bin/new_system.rs
@@ -0,0 +1,29 @@
+//! Entrypoint for exercising the new plugin/subsystem engine runtime.
+
+use anyhow::Context;
+use tracing::error;
+
+fn run() -> anyhow::Result<()> {
+    let mut builder = dirk_engine::Engine::builder();
+
+    builder.with_plugin(dirk_assets::AssetsPlugin)?;
+    builder.with_plugin(dirk_platform::PlatformPlugin)?;
+    builder.with_plugin(dirk_player::PlayerPlugin)?;
+    builder.with_plugin(dirk_world::WorldPlugin)?;
+    builder.with_plugin(dirk_renderer::RendererPlugin)?;
+
+    let engine = builder.build().context("build new engine")?;
+
+    engine.run().context("run new engine")?;
+    Ok(())
+}
+
+fn main() {
+    match run() {
+        Ok(()) => {}
+        Err(err) => {
+            error!("{err:#}");
+            panic!("Error: {err:#}");
+        }
+    }
+}
