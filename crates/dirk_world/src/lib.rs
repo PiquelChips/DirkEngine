c2a8e7ba427f45b2d0d0d059b0d7f5bde312c31c
piquel <piquel@piquel.fr>
2026-06-13 12:42:24 +0200

    port world to new system

--- crates/dirk_world/src/lib.rs
+++ crates/dirk_world/src/lib.rs
@@ -1,12 +1,45 @@
 #![doc = include_str!("../README.md")]
 
+use dirk_assets::AssetRegistry;
 use dirk_universe::{Universe, UniverseBuilder};
 
 pub mod components;
 use components::ModelUploadSystem;
 
+/// Registers world-specific ECS systems.
+pub struct WorldPlugin;
+
+impl dirk_engine::EnginePlugin for WorldPlugin {
+    fn name(&self) -> &'static str {
+        "world"
+    }
+
+    fn build(&self, builder: &mut dirk_engine::EngineBuilder) -> anyhow::Result<()> {
+        builder.with_plugin(dirk_assets::AssetsPlugin)?;
+
+        builder.add_subsystem(|ctx| {
+            let assets = ctx.resource::<AssetRegistry>()?;
+            ctx.extend_universe(
+                Universe::builder().with_component_system(ModelUploadSystem::new(assets)),
+            );
+            Ok(WorldSubsystem)
+        });
+
+        Ok(())
+    }
+}
+
+struct WorldSubsystem;
+
+impl dirk_engine::Subsystem for WorldSubsystem {
+    fn name(&self) -> &'static str {
+        "world"
+    }
+}
+
 /// Creates a [`UniverseBuilder`] with all the systems used by the various
 /// utilities & types in this crate.
+// TODO: remove after updating engine
 #[must_use]
 pub fn universe_builder(assets: dirk_assets::AssetRegistry) -> UniverseBuilder {
     Universe::builder().with_component_system(ModelUploadSystem::new(assets))
