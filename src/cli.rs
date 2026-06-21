e329d1a45c30cbb2a3846038b7219bb334b549ba
piquel <piquel@piquel.fr>
2026-06-21 18:52:50 +0200

    add basic CLI entrypoint for feature parity

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,20 @@
 //! This crate contains the CLI entrypoint & management for the enhanced clap
 //! engine CLI.
 
+use anyhow::Context;
+
+use crate::DefaultPlugins;
+
 /// Runs the enhanced [`clap`] CLI.
+///
+/// # Errors
+///
+/// Returns any error that occurs while running the engine.
 pub fn run() -> anyhow::Result<()> {
+    let mut builder = dirk_engine::Engine::builder();
+    builder.with_plugin(DefaultPlugins)?;
+    let engine = builder.build().context("build new engine")?;
+
+    engine.run().context("run new engine")?;
     Ok(())
 }
