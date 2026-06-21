26b4dd82d690bf9c3fb7842f46810d0879851300
piquel <piquel@piquel.fr>
2026-06-21 15:05:35 +0200

    add MainMenu

--- crates/dirk_editor/src/lib.rs
+++ crates/dirk_editor/src/lib.rs
@@ -1,1 +1,26 @@
-//! This crate will hold all the traits & types required for editor functionality.
+//! Default editor feature package for `DirkEngine`.
+//!
+//! This also contains loads of utility functions for working with the Editor.
+
+struct MainMenu;
+
+impl EditorMenu for MainMenu {
+    fn descriptor(&self) -> EditorMenuDescriptor {
+        EditorMenuDescriptor {
+            title: "Main".to_owned(),
+        }
+    }
+
+    fn ui(
+        &mut self,
+        ui: &mut egui::Ui,
+        context: &mut EditorUiContext<'_>,
+        _editor: &mut EditorMenuContext<'_>,
+    ) -> anyhow::Result<()> {
+        if ui.button("Exit").clicked() {
+            context.handle.exit();
+            ui.close();
+        }
+        Ok(())
+    }
+}
