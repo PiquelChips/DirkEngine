e79a4470258d56b4a592312b6e1dbeb4777e37bb
piquel <piquel@piquel.fr>
2026-06-21 19:04:16 +0200

    add basic CLI to disable demo

--- src/cli.rs
+++ src/cli.rs
@@ -2,8 +2,16 @@
 //! engine CLI.
 
 use anyhow::Context;
+use clap::Parser;
 
-use crate::DefaultPlugins;
+#[derive(Parser, Debug)]
+#[command(name = "dirkengine")]
+#[command(about = "DirkEngine CLI")]
+struct Cli {
+    /// Enable debug logging.
+    #[arg(long = "no-demo")]
+    no_demo: bool,
+}
 
 /// Runs the enhanced [`clap`] CLI.
 ///
@@ -11,8 +19,22 @@ use crate::DefaultPlugins;
 ///
 /// Returns any error that occurs while running the engine.
 pub fn run() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
     let mut builder = dirk_engine::Engine::builder();
-    builder.with_plugin(DefaultPlugins)?;
+
+    #[cfg(feature = "editor")]
+    builder.with_plugin(dirk_editor::EditorPlugin)?;
+    builder.with_plugin(dirk_assets::AssetsPlugin)?;
+    builder.with_plugin(dirk_platform::PlatformPlugin)?;
+    builder.with_plugin(dirk_player::PlayerPlugin)?;
+    builder.with_plugin(dirk_world::WorldPlugin)?;
+    builder.with_plugin(dirk_renderer::RendererPlugin)?;
+
+    if !cli.no_demo {
+        builder.with_plugin(crate::demo::DemoPlugin)?;
+    }
+
     let engine = builder.build().context("build new engine")?;
 
     engine.run().context("run new engine")?;
